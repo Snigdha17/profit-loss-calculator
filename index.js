8fce36b14ed5744033451400843719260af4171d
Snigdha17 <snigdhu17@gmail.com>
2021-09-26 15:21:19 -0700

    style: Added styling

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-var initialPrice = document.querySelector("#initial-price");
-var quantity = document.querySelector("#quantity");
-var currentPrice = document.querySelector("#current-price");
 const submitButton = document.querySelector("#submit-btn");
 const outputArea = document.querySelector("#output");
 
 submitButton.addEventListener("click", calculateProfitOrLoss);
 
 function calculateProfitOrLoss() {
+  var initialPrice = document.querySelector("#initial-price");
+  var quantity = document.querySelector("#quantity");
+  var currentPrice = document.querySelector("#current-price");
+
   if (!validateInput(initialPrice.value, quantity.value, currentPrice.value)) {
     return;
   }
+
   initialPrice = Number(initialPrice.value).toFixed(2);
   quantity = Number(quantity.value).toFixed(2);
   currentPrice = Number(currentPrice.value).toFixed(2);
@@ -23,16 +25,18 @@ function calculateProfitOrLoss() {
       initialPrice
     ).toFixed(2);
     setOutput(
-      `Your profit is ${profit} and your profit percentage is ${profitPercentage}%`
+      `Your profit is ${profit} and your profit percentage is ${profitPercentage}%`,
+      "PROFIT"
     );
   } else if (currentPrice < initialPrice) {
     var loss = calculateLoss(initialPrice, currentPrice, quantity).toFixed(2);
     var lossPercentage = calculateLossPercentage(loss, initialPrice).toFixed(2);
     setOutput(
-      `Your loss is ${loss} and your loss percentage is ${lossPercentage}%`
+      `Your loss is ${loss} and your loss percentage is ${lossPercentage}%`,
+      "LOSS"
     );
   } else {
-    setOutput("No Harm, No Foul!");
+    setOutput("No Harm, No Foul!", "EQUAL");
   }
 }
 
@@ -40,7 +44,7 @@ function validateInput(initialPrice, quantity, currentPrice) {
   if (initialPrice && quantity && currentPrice) {
     return true;
   }
-  setOutput("Please enter all the fields");
+  setOutput("Please enter all the fields", "MISSING");
   return false;
 }
 
@@ -60,6 +64,22 @@ function calculateLossPercentage(loss, initialPrice) {
   return (loss / initialPrice) * 100;
 }
 
-function setOutput(text) {
+function setOutput(text, STATUS) {
   outputArea.innerText = text;
+
+  switch (STATUS) {
+    case "PROFIT":
+      outputArea.style.color = "#70e000";
+      break;
+    case "LOSS":
+      outputArea.style.color = "red";
+      break;
+    case "MISSING":
+      outputArea.style.color = "gray";
+      break;
+    case "EQUAL":
+      outputArea.style.color = "orange";
+      break;
+    default:
+  }
 }
